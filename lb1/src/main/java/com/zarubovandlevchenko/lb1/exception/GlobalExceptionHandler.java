7bbc678dba3722cbf5bce8d646fdc4bf695cb698
byrybdyk <90798897+byrybdyk@users.noreply.github.com>
2025-04-12 13:17:55 +0300

    fixx: add exception

--- lb1/src/main/java/com/zarubovandlevchenko/lb1/exception/GlobalExceptionHandler.java
+++ lb1/src/main/java/com/zarubovandlevchenko/lb1/exception/GlobalExceptionHandler.java
@@ -2,6 +2,7 @@ package com.zarubovandlevchenko.lb1.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -32,5 +33,11 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(IllegalArgumentException.class)
     public ResponseEntity<String> handleIllegalArgument(IllegalArgumentException ex) {
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ex.getMessage());
+    }
+
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity<String> handleInvalidBoolean(HttpMessageNotReadableException ex) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .body("Invalid value for parameter");
     }
 }
